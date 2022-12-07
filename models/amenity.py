d237683cf55be7d76e4a4b75a7d4d67a229d3473
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-12-07 20:41:27 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
-
-"""
-Amenity Class:
-    Inherits from BaseModel and Base, defines two new class attributes
-"""
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models import storage_type
 from sqlalchemy import Column, String
-from os import getenv
 
 
 class Amenity(BaseModel, Base):
-    """
-    Represents Amenities available to users
-    """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "amenities"
+    '''amenity class'''
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
         name = Column(String(128), nullable=False)
     else:
         name = ""
-
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes the class with args and kwargs
-        """
-        super().__init__(*args, **kwargs)
